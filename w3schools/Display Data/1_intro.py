4eb2adef41a247ccbb80fc5c041ce7dd5b87eedf
H.M.Nizum <hmnizum1714032@gmail.com>
2024-07-07 12:12:32 +0600

    [ADD] - Introduction to pandas

--- w3schools/Display Data/1_intro.py
+++ w3schools/Display Data/1_intro.py
@@ -1,1 +1,19 @@
-import pandas as pd 
+1) What is Pandas?
+    Pandas is a Python library used for working with data sets.
+    It has functions for analyzing, cleaning, exploring, and manipulating data.
+    The name "Pandas" has a reference to both "Panel Data", and "Python Data Analysis"
+
+2) Why Use Pandas?
+    Pandas allows us to analyze big data and make conclusions based on statistical theories.
+    Pandas can clean messy data sets, and make them readable and relevant. Relevant data is very important in data science.
+
+3) Installation of Pandas 
+      >> pip install pandas
+
+4) Import Pandas 
+    import pandas as pd
+      [as : alias: In Python alias are an alternate name for referring to the same thing.]
+
+5) Checking Pandas Version
+    import pandas as pd
+    print(pd.__version__)
